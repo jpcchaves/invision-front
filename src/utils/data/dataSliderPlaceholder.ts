bc5c1f55e8d8b4ad3360f32727c71332bcb2366c
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2023-01-18 16:57:47 -0300

    refactor: change data slider placeholder

--- src/utils/data/dataSliderPlaceholder.ts
+++ src/utils/data/dataSliderPlaceholder.ts
@@ -6,12 +6,12 @@ export const dataSliderPlaceholder =
 	},
 	{
 		title: "Lorem Ipsum 2",
-		body: "g elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+		body: "amg elised do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim venit, "
 	},{
 		title: "Lorem Ipsum 3",
-		body: "g elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+		body: "ig eleniit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minm vam"
 	},{
 		title: "Lorem Ipsum 4",
-		body: "g elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+		body: "g ed od tempor incididunt ut l enim ad minim veniaelt, sdo eiiuabore et dolore magna aliqua. Utsmm"
 	}
  ]
