2a73f8a37249770381ac8d97c4d4cf470aff5564
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2023-01-18 22:25:48 -0300

    feat: add form validation on register page

--- src/modules/auth/pages/register/index.tsx
+++ src/modules/auth/pages/register/index.tsx
@@ -1,79 +1,26 @@
-import { Link } from 'react-router-dom';
-import GoogleLogo from '../../../../assets/google-logo.svg';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import RegisterView from './view';
 
 const RegisterPage = () => {
-	return (
-		<div className="w-[100%] max-w-full md:w-[50%] md:h-screen pb-48">
-			<div className="font-black text-black text-3xl h-[80px] flex items-center justify-end pr-6">
-				<div>Invision</div>
-			</div>
-			<div>
-				<div className="flex items-center justify-center w-[100%]">
-					<h3>Getting Started</h3>
-				</div>
-				<div>
-					<form className="flex flex-col w-[100%] md-[80%] px-12 py-2">
-						<span className="text-sm">Full name</span>
-						<input
-							type="text"
-							className="mb-5 border-b border-b-slate-300 p-2 focus:outline-none"
-							placeholder="Enter your full name"
-						/>
-						<span className="text-sm">Users name or Email</span>
-						<input
-							type="text"
-							className="mb-5 border-b border-b-slate-300 p-2 focus:outline-none"
-							placeholder="Enter your user name or email"
-						/>
-						<span className="text-sm focus:outline-none">Create Password</span>
-						<input
-							type="password"
-							className="mb-5 border-b border-b-slate-300 p-2 focus:outline-none"
-							placeholder="Enter your password"
-						/>
+	const validation = useFormik({
+		enableReinitialize: true,
+		initialValues: {
+			fullName: '',
+			email: '',
+			password: '',
+		},
+		validationSchema: Yup.object().shape({
+			fullName: Yup.string().required('Full name is a required field'),
+			email: Yup.string()
+				.required('Email is a required field')
+				.email('Please, use a valid email'),
+			password: Yup.string().required('Password is a required field'),
+		}),
+		onSubmit: (values) => alert(values),
+	});
 
-						<div className="flex items-center justify-center mt-4">
-							<button className="border-none bg-[#707070] text-white py-3 px-12 rounded-full">
-								Sign up
-							</button>
-						</div>
-						<div className="flex items-baseline justify-center mt-4">
-							<span className="border-b border-b-slate-300 inline-block w-[100px]"></span>
-							<small className="px-2">or</small>
-							<span className="border-b border-b-slate-300 inline-block w-[100px]"></span>
-						</div>
-						<div className="flex items-center justify-center mt-4">
-							<button className="border-none bg-[#FFFFFF] text-black font-semibold py-3 px-12 rounded-full flex gap-2 items-center justify-center shadow-lg">
-								<img src={GoogleLogo} width={20} height={20} />
-								Sign up with Google
-							</button>
-						</div>
-						<div className="flex flex-col gap-4 items-center justify-center mt-8 w-[100%] tracking-widest text-center m-auto">
-							<p className="text-sm">
-								By signing up, you agree to <strong>Invision</strong>{' '}
-								<Link to="/register" className="underline text-[#A9C5BA]">
-									Terms Of Conditions
-								</Link>{' '}
-								and{' '}
-								<Link to="/register" className="underline text-[#A9C5BA]">
-									Privacy Policy
-								</Link>{' '}
-								<Link to="/register" className="underline text-[#A9C5BA]">
-									Create Account
-								</Link>
-							</p>
-							<p className="text-sm">
-								Already <strong>Invision</strong> on?{' '}
-								<Link to="/" className="underline text-[#A9C5BA]">
-									Log in
-								</Link>
-							</p>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	);
+	return <RegisterView validation={validation} />;
 };
 
 export default RegisterPage;
---
+++ src/modules/auth/pages/register/view.tsx
@@ -0,0 +1,148 @@
+import { Form, Formik } from 'formik';
+import { FormikHelpers, FormikValues } from 'formik/dist/types';
+import { Link } from 'react-router-dom';
+import GoogleLogo from '../../../../assets/google-logo.svg';
+
+interface RegisterViewI {
+	validation: FormikValues;
+}
+
+const RegisterView = ({ validation }: RegisterViewI) => {
+	return (
+		<div className="w-[100%] max-w-full md:w-[50%] md:h-screen pb-48">
+			<div className="font-black text-black text-3xl h-[80px] flex items-center justify-end pr-6">
+				<div>Invision</div>
+			</div>
+			<div>
+				<div className="flex items-center justify-center w-[100%]">
+					<h3>Getting Started</h3>
+				</div>
+				<div>
+					<Formik
+						initialValues={{
+							fullName: '',
+							email: '',
+							password: '',
+						}}
+						onSubmit={function (
+							values: { fullName: string; email: string; password: string },
+							formikHelpers: FormikHelpers<{
+								fullName: string;
+								email: string;
+								password: string;
+							}>
+						): void | Promise<any> {
+							throw new Error('Function not implemented.');
+						}}
+					>
+						<Form
+							onSubmit={(e) => {
+								e.preventDefault();
+								validation.handleSubmit();
+								return false;
+							}}
+							className="flex flex-col w-[100%] md-[80%] px-12 py-2"
+						>
+							<span className="text-sm">Full name</span>
+							<input
+								type="text"
+								name="fullName"
+								className={`p-2 focus:outline-none rounded ${
+									validation.touched.fullName && validation.errors.fullName
+										? 'border border-red-500'
+										: 'border-b border-b-slate-300 '
+								}`}
+								placeholder="Enter your full name"
+								onChange={validation.handleChange}
+								value={validation.values.fullName || ''}
+							/>
+							{validation.touched.fullName && validation.errors.fullName ? (
+								<span className="text-red-500 mb-1">
+									{validation.errors.fullName}
+								</span>
+							) : null}
+
+							<span className="text-sm">Users name or Email</span>
+							<input
+								type="text"
+								name="email"
+								className={`p-2 focus:outline-none rounded ${
+									validation.touched.email && validation.errors.email
+										? 'border border-red-500'
+										: 'border-b border-b-slate-300 '
+								}`}
+								placeholder="Enter your user name or email"
+								onChange={validation.handleChange}
+								value={validation.values.email || ''}
+							/>
+							{validation.touched.email && validation.errors.email ? (
+								<span className="text-red-500 mb-1">
+									{validation.errors.email}
+								</span>
+							) : null}
+							<span className="text-sm focus:outline-none">
+								Create Password
+							</span>
+							<input
+								name="password"
+								type="password"
+								className={`p-2 focus:outline-none rounded ${
+									validation.touched.password && validation.errors.password
+										? 'border border-red-500'
+										: 'border-b border-b-slate-300 '
+								}`}
+								placeholder="Enter your password"
+								onChange={validation.handleChange}
+								value={validation.values.password || ''}
+							/>
+							{validation.touched.password && validation.errors.password ? (
+								<span className="text-red-500 mb-1">
+									{validation.errors.password}
+								</span>
+							) : null}
+							<div className="flex items-center justify-center mt-4">
+								<button className="border-none bg-[#707070] text-white py-3 px-12 rounded-full">
+									Sign up
+								</button>
+							</div>
+							<div className="flex items-baseline justify-center mt-4">
+								<span className="border-b border-b-slate-300 inline-block w-[100px]"></span>
+								<small className="px-2">or</small>
+								<span className="border-b border-b-slate-300 inline-block w-[100px]"></span>
+							</div>
+							<div className="flex items-center justify-center mt-4">
+								<button className="border-none bg-[#FFFFFF] text-black font-semibold py-3 px-12 rounded-full flex gap-2 items-center justify-center shadow-lg">
+									<img src={GoogleLogo} width={20} height={20} />
+									Sign up with Google
+								</button>
+							</div>
+							<div className="flex flex-col gap-4 items-center justify-center mt-8 w-[100%] tracking-widest text-center m-auto">
+								<p className="text-sm">
+									By signing up, you agree to <strong>Invision</strong>{' '}
+									<Link to="/register" className="underline text-[#A9C5BA]">
+										Terms Of Conditions
+									</Link>{' '}
+									and{' '}
+									<Link to="/register" className="underline text-[#A9C5BA]">
+										Privacy Policy
+									</Link>{' '}
+									<Link to="/register" className="underline text-[#A9C5BA]">
+										Create Account
+									</Link>
+								</p>
+								<p className="text-sm">
+									Already <strong>Invision</strong> on?{' '}
+									<Link to="/" className="underline text-[#A9C5BA]">
+										Log in
+									</Link>
+								</p>
+							</div>
+						</Form>
+					</Formik>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default RegisterView;
